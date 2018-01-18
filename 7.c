edd55b0110c9d6400dfe907c6bca2e96c69a92e0
anubbjhav <35500360+anubbjhav@users.noreply.github.com>
2018-01-18 01:23:10 -1200

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+    printf("Hello\n");
+    }
+    return 0;
+}
