08a39d65b5c1e00d5c552e0d58ebab782b058fdf
anubbjhav <35500360+anubbjhav@users.noreply.github.com>
2018-01-20 05:39:31 -1200

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+  int n,m;
+  printf("enter the two numbers\n");
+  scanf("%d%d",&n,&m);
+  for(int i=n+1;i<=m-1;i++)
+  {
+      if(i%2!=0)
+      {
+          printf("%d\n",i);
+      }
+      
+  }
+
+    return 0;
+}
