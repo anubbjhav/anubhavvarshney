3ed343fa80e78ba64fcc532ad19b207cbdd59883
anubbjhav <35500360+anubbjhav@users.noreply.github.com>
2018-01-20 05:27:29 -1200

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int n,count=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+        }
+        
+    }
+    if(count==2)
+    {
+        printf("it is prime number");
+    }
+    else
+    {
+        printf("it is not prime");
+    }
+    
+
+    return 0;
+}
+
+
+
