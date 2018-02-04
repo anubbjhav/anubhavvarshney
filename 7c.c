382d6e0b4bde238eb3e4d1c51728aedd68d5e478
anubbjhav <35500360+anubbjhav@users.noreply.github.com>
2018-02-03 23:07:58 -1200

    Create 7c.c

---
+++ 7c.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{ 
+    char ch[100];
+int len,i,temp;
+printf("enter string\n");
+scanf("%s",&ch);
+len =strlen(ch);
+for(i=0;i<len;i=i+2)
+{
+   temp=ch[i+1];
+   ch[i+1]=ch[i];
+   ch[i]=temp;
+ 
+}
+
+printf("%s",ch);
+  return 0;
+}
