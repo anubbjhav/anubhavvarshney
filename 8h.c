376530e25da0a90525521adf6a535aa2f8156d64
anubbjhav <35500360+anubbjhav@users.noreply.github.com>
2018-02-03 23:35:50 -1200

    Create 8h.c

---
+++ 8h.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char a[100];
+    int p=0 , i;
+    printf("enter string\n");
+    gets(a);
+    for(i=0;i<a[i]!='\0';i++)
+    {
+	if(i==0)
+	{
+		a[i] = a[i] - 32;
+	}
+	if(a[i] == ' ')
+	{
+		a[i+1] = a[i+1] - 32 ; 
+	}
+	printf("%c",a[i]);
+}
+
+}
