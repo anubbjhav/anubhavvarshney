c7a5ddb6301e803bc194bbc9444f996d7f6a9def
anubbjhav <35500360+anubbjhav@users.noreply.github.com>
2018-02-03 20:07:14 -1200

    Rename 3.c to 3a.c

---
+++ 3a.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int n,r=0
+  printf("enter n");
+  scanf("%d",&n);
+  while(n>0)
+  {
+      
+      r=r*10+n%10;
+      n=n/10;
+  }
+  
+  printf("reverse is %d",r);
+
+    return 0;
+}
+
